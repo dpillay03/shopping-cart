e1b3f5fb618f1ce08d1fb738775c1c7631219adc
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-09-24 16:13:52 -0700

    created json file with products

---
+++ client/src/components/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Homepage extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <React.Fragment>
+            <h1>Products</h1>
+        </React.Fragment>
+    }
+}
